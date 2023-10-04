6c66d8c305c633597d2790a4ec1b28c7bad5b8f0
unknown <176243@BME.HU>
2023-10-04 09:00:55 +0200

    jo

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -23,7 +23,7 @@ public class GT4500Test {
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    assertEquals(false, result);
   }
 
   @Test
